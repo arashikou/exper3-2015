62a29ebcb39d521b07e5a3fb4c67ab9ac8a84dbd
John W. Bruce <arashikou@gmail.com>
2015-07-12 17:06:32 -0700

    Remove Gulpfile CoffeeScript Bootstrapping
    Apparently this is not needed anymore.

--- gulpfile.js
+++
@@ -1,4 +0,0 @@
-// This is just a small script that loads the Node.js CoffeeScript support and then passes
-// execution to the real gulpfile: gulpfile.coffee
-require('coffee-script/register');
-require('./gulpfile.coffee');
